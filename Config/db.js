ed65065946d0c18ce139046049291626ed533f6f
Chandrakant Pawar <chandrakantpawar590@gmail.com>
2024-09-04 02:27:56 +0530

    update

--- Config/db.js
+++ Config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://chandrakantpawar590:8nyVDvWOy6z9i371@cluster0.w9z8e.mongodb.net/Expense-tracker"
-  )
+  .connect("mongodb://localhost:27017/Expense-tracker")
   .then(() => console.log("Database Connected Successfully...."))
   .catch((err) => {
     console.log("Database Connection Failed..!", err);
